ae460fbfbcb6c260ee1fa78fc6a1487900259061
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-24 15:20:16 +0000

    removed database setup from within the Space class

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -1,5 +1,3 @@
-require_relative 'database_connection'
-
 class Spaces
 
   attr_reader :name, :description, :location, :price
@@ -12,17 +10,13 @@ class Spaces
   end
 
   def self.create(name:, description:, location:, price:)
-    DatabaseConnection.setup("makers_bnb")
-    DatabaseConnection.setup("makers_bnb_test") if ENV['ENVIRONMENT'] == 'test'
-    DatabaseConnection.query(sql: "INSERT INTO spaces (name, description, location, price)
+    DatabaseConnection.query("INSERT INTO spaces (name, description, location, price)
                               VALUES ('#{name}', '#{description}', '#{location}', #{price});")
     Spaces.new(name, description, location, price)
   end
 
   def self.retrieve_availables
-    DatabaseConnection.setup("makers_bnb")
-    DatabaseConnection.setup("makers_bnb_test") if ENV['ENVIRONMENT'] == 'test'
-    result = DatabaseConnection.query(sql: "SELECT * FROM spaces;")
+    result = DatabaseConnection.query("SELECT * FROM spaces;")
     result
   end
 end
--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -2,15 +2,15 @@ describe DatabaseConnection do
   describe '.setup' do
     it 'establishes a connection to the test db using PG' do
       expect(PG).to receive(:connect).with(dbname: 'makers_bnb_test')
-      DatabaseConnection.setup('makers_bnb_test')
+      DatabaseConnection.setup(dbname: 'makers_bnb_test')
     end
   end
 
   describe '.query' do
     it 'calls the exec method with the sql passed in' do
-      connection = DatabaseConnection.setup('makers_bnb_test')
+      connection = DatabaseConnection.setup(dbname: 'makers_bnb_test')
       expect(connection).to receive(:exec).with('SELECT * FROM users;')
-      DatabaseConnection.query(sql: 'SELECT * FROM users;')
+      DatabaseConnection.query('SELECT * FROM users;')
     end
   end
 end
