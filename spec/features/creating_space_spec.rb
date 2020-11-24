e47a447562b823d4f3b7a5e35ab25a2c29d79da2
adam2021 <adam.patel101@outlook.com>
2020-11-24 17:46:52 +0000

    add new property form

--- spec/features/creating_space_spec.rb
+++ spec/features/creating_space_spec.rb
@@ -1,7 +1,25 @@
 require_relative '../../controllers/spaces_controller'
 
 feature "Creates a new Space for a user" do
-  scenario "it displays 'MakersBnB - List your Property'" do
 
+  scenario "users can click a link from the homepage to access the 'add space' form" do
+    visit('/')
+    expect(page).to have_link 'List your property'
+    click_link 'List your property'
+    expect(page).to have_content 'List your property on MakersBnB'
   end
+
+  scenario "users can enter basic details of their property into the 'add space' form" do
+    visit('/add_space')
+    fill_in 'property_name', with: 'Hidden Gem of Beverly Hills'
+    fill_in 'property_description', with: 'A luxurious villa in Beverly Hills'
+    fill_in 'property_location', with: 'Los Angeles, Beverly Hills'
+    fill_in 'property_price', with: '300'
+    click_button 'Post'
+    expect(page).to have_content("Hidden Gem of Beverly Hills")
+    expect(page).to have_content("A luxurious villa in Beverly Hills")
+    expect(page).to have_content("Los Angeles, Beverly Hills")
+    expect(page).to have_content(300)
+  end
+
 end
