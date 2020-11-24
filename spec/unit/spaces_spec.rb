701e4077ccc79422c7d80012c026c88b08ec0d49
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-24 14:01:40 +0000

    added expectations in spaces_spec file

--- spec/unit/spaces_spec.rb
+++ spec/unit/spaces_spec.rb
@@ -9,6 +9,10 @@ describe Spaces do
     it "inserts its arguments into the spaces table" do
       result = Spaces.create(description: "A luxurious villa in Beverly Hills", name: "Hidden Gem of Beverly Hills", location: "Los Angeles, Beverly Hills", price: 300)
       expect(result).to be_instance_of(described_class)
+      expect(result.name).to eq("Hidden Gem of Beverly Hills")
+      expect(result.description).to eq("A luxurious villa in Beverly Hills")
+      expect(result.location).to eq("Los Angeles, Beverly Hills")
+      expect(result.price).to eq(300)
     end
   end
 end
