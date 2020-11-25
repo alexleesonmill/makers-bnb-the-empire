4fea3b21d90b3ae90432f52bcf9a1657483bab6b
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-25 12:02:19 +0000

    User is redirected to login if not logged in

--- controllers/spaces_controller.rb
+++ controllers/spaces_controller.rb
@@ -1,14 +1,27 @@
 require_relative '../lib/spaces'
 
 class MakersBnB < Sinatra::Base
-
   get '/listings' do
-    @spaces = Space.retrieve_available
-    erb :listings
+    @user = User.find(id: session[:user_id])
+
+    if @user 
+      @spaces = Space.retrieve_available
+      erb :listings
+    else
+      flash[:notice] = 'Please log in to view listings'
+      redirect('/sessions/new')
+    end
   end
 
   get '/add_space' do
-    erb(:add_new_property)
+    @user = User.find(id: session[:user_id])
+
+    if @user
+      erb(:add_new_property)
+    else
+      flash[:notice] = 'Please log in to add a space'
+      redirect('/sessions/new')
+    end
   end
 
   post '/listings/new' do
@@ -16,5 +29,4 @@ class MakersBnB < Sinatra::Base
     location: params[:property_location], price: params[:property_price])
     redirect('/listings')
   end
-
 end
--- spec/features/listing_spaces_spec.rb
+++ spec/features/listing_spaces_spec.rb
@@ -2,13 +2,23 @@ require_relative '../../controllers/spaces_controller'
 
 feature "displaying spaces on MakersBnB" do
   scenario "displays 'Makers BnB Spaces' on /listings route" do
-    visit '/listings'
+    login_to_dashboard
+    click_link('Listings')
+
     expect(page).to have_content("MakersBnB Spaces")
   end
 
   scenario "display the listings list on /listings route" do
-    Space.create(description: "A luxurious villa in Beverly Hills", name: "Hidden Gem of Beverly Hills", location: "Los Angeles, Beverly Hills", price: 300)
-    visit '/listings'
+    Space.create(
+      description: "A luxurious villa in Beverly Hills", 
+      name: "Hidden Gem of Beverly Hills", 
+      location: "Los Angeles, Beverly Hills", 
+      price: 300
+    )
+
+    login_to_dashboard
+    click_link('Listings')
+
     expect(page).to have_content("Hidden Gem of Beverly Hills")
     expect(page).to have_content("A luxurious villa in Beverly Hills")
     expect(page).to have_content("Los Angeles, Beverly Hills")
